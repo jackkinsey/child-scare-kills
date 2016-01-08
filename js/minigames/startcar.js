aaa889d2e47ebb43f3a41c0ab198b1a2d6dc1d6c
Liam <liam.mcfalls@gmail.com>
2016-01-08 17:09:04 -0500

    updates to startcar.js

--- js/minigames/startcar.js
+++ js/minigames/startcar.js
@@ -3,25 +3,39 @@
  */
 
 var startCar = new Game({
-    
+    keyhole: {
+        posX: 100,
+        posY: 100,
+        zIndex: -1,
+        widthX: 128,
+        widthY: 128,
+        rot: 0,
+        on: false,
+        src: "img/keyhole.png",
+        spr: null
+    }
 })
 
 feedBaby.build = function(context) {
-    
+    this.props.keyhole.spr = context.addSprite(this.props.keyhole.widthX, this.props.baby.widthY, this.props.baby.src, {x: this.props.baby.posX,
+                                                                                                                  y: this.props.baby.posY,
+                                                                                                                  z: this.props.baby.zIndex});
 };
 
 feedBaby.update = function(mouse, delta) {
-    if (Math.sqrt(Math.pow(this.props.food.posX - mouse.mouseX, 2) + Math.pow(this.props.food.posY - mouse.mouseY, 2)) < this.props.tolerance){
-        this.props.food.lift = mouse.mouseState == "down" ? true : false;
+    if (keyboard.d == down && this.props.keyhole.rot < Math.PI / 2) {
+        this.props.keyhole.rot += (Math.PI / 16)*delta;
+        this.props.keyhole.spr.material.rotation = this.props.keyhole.rot;
+    } else if (keyboard.d == up && this.props.keyhole.rot > 0) {
+        this.props.keyhole.rot -= (Math.PI / 16)*delta;
+        this.props.keyhole.spr.material.rotation = this.props.keyhole.rot;
+    } else {
+        this.props.keyhole.on = (this.props.keyhole == 0) ? false : true;
     }
-    if (this.props.food.lift) {
-        this.props.food.posX = mouse.mouseX;
-        this.props.food.posY = mouse.mouseY;
-        
-        this.props.food.spr.position.set(this.props.food.posX, this.props.food.posY, 0);
-    }
-    if (Math.sqrt(Math.pow(this.props.food.posX - this.props.baby.posX,2) + Math.pow(this.props.food.posY - this.props.baby.posY,2)) < 32 && !this.props.baby.lift) {
-        console.log("You fed baby!");
+    
+    if (keyboard.space == down) {
+        if (this.props.keyhole.on); //win
+        else; //lose
     }
 };
 
