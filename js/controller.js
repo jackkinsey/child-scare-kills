76c385494c0eeb8296d6e1783196f7e182f3d13c
Liam <liam.mcfalls@gmail.com>
2016-01-10 16:42:15 -0500

    Added jumper cable minigame.

--- js/controller.js
+++ js/controller.js
@@ -16,7 +16,7 @@ Controller.prototype = {
     },
     play: function(context){
         this.timer.start();
-        this.games.buildGame(2, context);
+        this.games.buildGame(3, context);
     },
     clickHandler: function(event, context){
         if(event.target.id == "play-button"){
